01374b5c8ab1d40963482e3805efbe5e6ce44b14
Renan <renan.fachin@hotmail.com>
2023-09-22 18:44:42 -0300

    config: loading fonts from external sources

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
+// Componentes react native
+import { ActivityIndicator } from 'react-native'
+
+// Componentes personalizados
 import { Groups } from '@screens/Groups';
+
+// Themes
 import { ThemeProvider } from 'styled-components'
 import theme from '@theme/index';
 
+// Fontes
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  // Caso fontsLoaded for true, quer dizer que as fonts já foram carregadas
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
