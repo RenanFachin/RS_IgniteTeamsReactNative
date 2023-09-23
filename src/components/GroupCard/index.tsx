0da81a150aa5fff1ee1893b44c6ce909f25346a3
Renan <renan.fachin@hotmail.com>
2023-09-23 17:48:44 -0300

    feat: create groupCard component

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container, Title, Icon } from './styles'
+
+type Props = TouchableOpacityProps & {
+  title: string;
+}
+
+
+export function GroupCard({ title, ...props }: Props) {
+  return (
+    <Container {...props}>
+      <Icon />
+
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
