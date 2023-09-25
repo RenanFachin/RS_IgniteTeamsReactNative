c76e1795d6e6fc90b7809eee750baaa2524fbbfc
Renan <renan.fachin@hotmail.com>
2023-09-24 23:13:34 -0300

    style: improve Players screen style

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { Container, Form, HeaderList, PlayersCount } from './styles'
 
-import { Header } from '@components/Header'
-import { Highlight } from '@components/Highlight'
-import { ButtonIcon } from '@components/ButtonIcon'
 import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
+import { Header } from '@components/Header'
+import { Button } from '@components/Button'
+import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
+import { ButtonIcon } from '@components/ButtonIcon'
 import { PlayerCard } from '@components/PlayerCard'
 
 export function Players() {
-  const [players, setPlayers] = useState<string[]>(['Renan', 'Apolo'])
+  // 'Renan', 'Apolo', 'Tom', 'Momoa'
+  const [players, setPlayers] = useState<string[]>([])
   const [team, setTeam] = useState('Time A')
 
   return (
@@ -60,9 +63,23 @@ export function Players() {
           <PlayerCard
             iconName='person'
             playerName={item}
-            onRemove={() => {}}
+            onRemove={() => { }}
           />
         )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => <ListEmpty message='Não há pessoas neste time.' />}
+        contentContainerStyle={
+          [
+            { paddingBottom: 60 },
+            players.length === 0 && { flex: 1 } // faz o component de listEmpty ocupar todo o espaço disponível qnd o total de jogadores em um time for vazio (igual a 0)
+          ]
+        }
+      />
+
+
+      <Button 
+        title='Remover Turma'
+        buttonType='SECONDARY'
       />
 
     </Container>
