5c6b7158c3327aff7016f832459eafb67090c540
Renan <renan.fachin@hotmail.com>
2023-09-27 15:25:06 -0300

    feat: exception handling

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { Container, Content, Icon } from './styles'
 import { useNavigation } from '@react-navigation/native'
 
@@ -7,6 +8,7 @@ import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
 
 export function NewGroup() {
   const navigation = useNavigation()
@@ -15,12 +17,22 @@ export function NewGroup() {
 
   async function handleNewPlayer() {
     try {
+      if(groupName.trim().length === 0) {
+        // caso não tenha contéudo
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
       // salvando no storage
       await groupCreate(groupName)
-
       navigation.navigate('players', { group: groupName })
+
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
     }
   }
 
